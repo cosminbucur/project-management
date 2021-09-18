13a1240519b65908789146aaa4d0d375d444d363
cosmin bucur <cosmin.bucur@kambi.com>
2021-09-18 09:07:59 +0300

    DEV-20 enable js code

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,15 +1,31 @@
 (function(){
+    'use strict';
     console.log("Application started...");
 })();
 
 // activate toasts
-document.getElementById("toast-btn").onclick = function() {
-    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-    var toastList = toastElList.map(function(toastEl) {
-        // creates an array of toasts with default options
-        return new bootstrap.Toast(toastEl)
-    });
-    toastList.forEach(toast => toast.show());
+//document.getElementById("toast-btn").onclick = function() {
+//    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+//    var toastList = toastElList.map(function(toastEl) {
+//        // creates an array of toasts with default options
+//        return new bootstrap.Toast(toastEl)
+//    });
+//    toastList.forEach(toast => toast.show());
+//
+//    console.log(toastList);
+//};
 
-    console.log(toastList);
-};
+window.addEventListener('load', function() {
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.getElementsByClassName('needs-validation');
+// Loop over them and prevent submission
+var validation = Array.prototype.filter.call(forms, function(form) {
+  form.addEventListener('submit', function(event) {
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+  }, false);
+});
+}, false);
