f1c9b0a1c43d56dd773e7a554d24ea98490f7ab0
AlexD <asdragomir@gmail.com>
2021-09-21 19:18:10 +0300

    DEV-24 create task solved issue

--- src/main/java/com/sda/project/management/service/TaskService.java
+++ src/main/java/com/sda/project/management/service/TaskService.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,6 +23,12 @@ public class TaskService {
 
     public void save(Task task) {
         log.info("save task {}", task);
+        if(task.getAssignee().getId() == null){
+            task.setAssignee(null);
+        }
+        if(task.getSprint().getId() == null){
+            task.setSprint(null);
+        }
         taskRepository.save(task);
     }
 
